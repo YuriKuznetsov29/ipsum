b20d9f0788cf383d468250c6f34f09d33bdd28c8
Yuri Kuznetsov <playeer2905@gmail.com>
2024-03-20 19:09:08 +0300

    add jobs, news, about section

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,73 @@
-import * as flsFunctions from "./modules/functions.js"
-import Swiper from "swiper/bundle"
+import * as flsFunctions from "./modules/functions.js";
+import Swiper from "swiper/bundle";
 
-const swiper = new Swiper(".companies__slider", {
+const companiesSlider = new Swiper(".companies__slider", {
     slidesPerView: "auto",
     spaceBetween: 30,
-    centeredSlides: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    navigation: {
+        nextEl: ".companies__slider__button-next",
+        prevEl: ".companies__slider__button-prev",
     },
-})
+});
 
-const element = document.querySelector(".companies__slider")
+const companiesSliderContainer = document.querySelector(".companies__slider");
 
-element.addEventListener("mouseover", () => {
-    swiper.autoplay.stop()
-})
+companiesSliderContainer.addEventListener("mouseover", () => {
+    companiesSlider.autoplay.stop();
+});
 
-element.addEventListener("mouseout", () => {
-    swiper.autoplay.start()
-})
+companiesSliderContainer.addEventListener("mouseout", () => {
+    companiesSlider.autoplay.start();
+});
 
-flsFunctions.isWebp()
+const newsSlider = new Swiper(".news__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".news__slider__button-next",
+        prevEl: ".news__slider__button-prev",
+    },
+});
+
+const newsSliderContainer = document.querySelector(".news__slider");
+
+newsSliderContainer.addEventListener("mouseover", () => {
+    newsSlider.autoplay.stop();
+});
+
+newsSliderContainer.addEventListener("mouseout", () => {
+    newsSlider.autoplay.start();
+});
+
+const aboutSlider = new Swiper(".about__slider", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".about__slider__button-next",
+        prevEl: ".about__slider__button-prev",
+    },
+});
+
+const aboutSliderContainer = document.querySelector(".about__slider");
+
+aboutSliderContainer.addEventListener("mouseover", () => {
+    aboutSlider.autoplay.stop();
+});
+
+aboutSliderContainer.addEventListener("mouseout", () => {
+    aboutSlider.autoplay.start();
+});
+
+flsFunctions.isWebp();
